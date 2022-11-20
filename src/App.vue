470a5c89f898109d054129bdaa49ae0a19b2200e
WithorCello <withorcello@gmail.com>
2022-11-20 14:53:50 -0400

    feat: add basic structure

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from "vue-router";
+import icon from "./assets/app-bar-icon.svg";
+
+const ico = icon;
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar flat class="bg-background text-h5">
+      <v-icon color="red"> {{ ico }}</v-icon>
+      Rick and Morty
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
