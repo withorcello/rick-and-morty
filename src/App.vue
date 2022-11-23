26d720830f063c5b540ff71d9a02f3a21df32a50
WithorCello <withorcello@gmail.com>
2022-11-22 21:05:53 -0400

    feat: add icon and function return to the wlecome page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <script setup>
 import { RouterView } from "vue-router";
-import icon from "./assets/app-bar-icon.svg";
-
-const ico = icon;
 </script>
 
 <template>
   <v-app>
-    <v-app-bar flat class="bg-background text-h5">
-      <v-icon color="red"> {{ ico }}</v-icon>
-      Rick and Morty
+    <v-app-bar flat class="bg-background px-4">
+      <v-icon color="primary" size="x-large" @click="$router.push('/')">
+        mdi-atom
+      </v-icon>
+      <span
+        class="mx-2 text-h5 font-weight-bold text-grey-lighten-2"
+        @click="$router.push('/')"
+      >
+        Rick and Morty
+      </span>
     </v-app-bar>
     <v-main>
       <RouterView />
     </v-main>
   </v-app>
 </template>
+
+<style setup>
+html {
+  overflow-y: auto;
+}
+</style>
